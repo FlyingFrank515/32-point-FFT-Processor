ab27ea4276eeb4de636f0220157cf736a5c7aac9
LungHuiWu <b07901019@ntu.edu.tw>
2021-04-14 19:31:57 +0800

    python 8bit

---
+++ tool/floating_to_8bit.py
@@ -0,0 +1,53 @@
+from __future__ import division
+import math
+import sys
+
+def twos_comp(x):
+    output = ''
+    i = 7
+    exp = 8
+    if x >= 0:
+        output = output + '0'
+    else:
+        output = output + '1'
+        x = x + 16
+
+    while(i > 0):
+        if(i == 3):
+            output = output + '_'
+        if(x >= exp):
+            x = x - exp
+            output = output + '1'
+        else:
+            output = output + '0'
+        exp = exp / 2
+        i = i - 1
+    output = output + '\n'
+    return output
+
+string_in = sys.argv[1]
+f = open(string_in, "r")
+points = f.read()
+f.close()
+
+# for i in points
+points_list = points.split('\n')
+points_list.pop(-1)
+
+
+pt_float = []
+
+for i in points_list:
+    pt_float.append(float(i))
+
+pt_final = []
+
+string_out = string_in[0:-6] + "_8bit" + string_in[-6:]
+f = open(string_out, 'w')
+for i in pt_float:
+    pt_final.append(twos_comp(i))
+    f.write(pt_final[-1])
+
+f.close()
+
+
