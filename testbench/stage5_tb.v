c44540050a0d39621f90dc4850ab94b52d6fd15b
FrankPan <a1687193@gmail.com>
2021-05-05 12:00:59 +0800

    5/5:stage_5 strange

---
+++ testbench/stage5_tb.v
@@ -0,0 +1,128 @@
+`timescale 1ns/10ps
+`define CYCLE    10           	         // Modify your clock period here
+
+`include "STAGE1.v"             
+
+// simulation commmand: ncverilog stage1_tb.v +access+r
+
+module STAGE1_tb;
+    
+    integer i, j, f;
+    reg clk, rst_n, valid, stop;
+    reg [33:0] before_ff [0:31];
+    reg [16:0] data_in_r, data_in_i;
+    wire [16:0] data_out_i, data_out_r;
+    wire finish;
+
+    STAGE5 test(
+        .clk(clk),
+        .rst_n(rst_n),
+        .valid_i(valid),
+        .data_in_r(data_in_r),
+        .data_in_i(data_in_i),
+
+        .valid_o(finish),
+        .data_out_r(data_out_r),
+        .data_out_i(data_out_i)
+    );     
+
+    initial	begin
+        $readmemb ("stage4_o.txt",  before_ff);
+        f = $fopen("stage5_o.txt","w");
+    end
+
+    initial begin
+        clk         = 1'b1;
+        rst_n         = 1'b1;  
+        valid       = 1'b0;
+        stop        = 1'b0;
+        i           = 0;
+        j           = 0;
+        #2.5 rst_n=1'b0;         
+        #2.5 rst_n=1'b1;
+
+    end
+
+    always begin #(`CYCLE/2) clk = ~clk; end
+
+    initial begin
+        $dumpfile("stage5");
+        $dumpvars;
+    end
+
+    always @(negedge clk)begin
+        if(i < 32) begin
+            valid = 1; 
+            data_in_r = before_ff[i][33:17];
+            data_in_i = before_ff[i][16:0];
+            i = i+1;      
+        end
+        else if(i < 42) begin
+            data_in_r = 0;
+            i = i+1;
+        end
+        else begin
+            data_in_r = 0;
+            stop = 1;
+        end
+    end
+
+    always @(negedge clk)begin
+        if(finish) begin
+            $fwrite(f,"%b_%b\n", data_out_r, data_out_i);
+            $display("Output %0d: Real->%b / Img->%b", j , data_out_r, data_out_i);
+            j = j + 1;
+        end
+
+    end
+
+    always @(posedge stop)begin
+        $fclose(f);
+        $finish;
+    end
+
+    // always @(posedge clk)begin
+        
+    //     if(dataout !== out_temp && out_temp!==16'h0000) begin
+    //         $display("ERROR at %d:output %h !=expect %h ",pattern_num-2, dataout, out_temp);
+    // 	    $fdisplay(out_f,"ERROR at %d:output %h !=expect %h ",pattern_num-2, dataout, out_temp);
+    //         err = err + 1 ;
+    //     end
+    //     pattern_num = pattern_num + 1; 
+    //     if(pattern_num === OUT_LENGTH)  over = 1'b1;
+    // end
+
+    // initial begin
+    //       @(posedge stop)      
+    //       if(over) begin
+    //          $display("---------------------------------------------\n");
+    //          if (err == 0)  begin
+    //             $display("All data have been generated successfully!\n");
+    // 			$display("You will get 80 score in this RTL!\n");
+    //             $display("-------------------PASS-------------------\n");
+    //          end
+    //          else begin
+    //             $display("There are %d errors!\n", err);
+    // 			$display("You will get %d score in this RTL!\n", 80-err);
+    // 		 end
+    //             $display("---------------------------------------------\n");
+    //       end
+    //       else begin
+    //         $display("---------------------------------------------\n");
+    //         $display("Error!!! There is no any data output ...!\n");
+    //         $display("-------------------FAIL-------------------\n");
+    //         $display("---------------------------------------------\n");
+    //       end
+    //       $finish;
+    // end
+   
+endmodule
+
+
+
+
+
+
+
+
+
---
+++ tool/17bit_final_fixed_to_double.py
@@ -0,0 +1,45 @@
+from __future__ import division
+import math
+import sys
+
+def double(x):
+    # total i+1 bit, behind . :j, front of . : i+1-j
+    i = 16
+    j = 6
+    k = i - j
+    exp = 0.5**j
+    out = 0
+    while(i>0):
+        if(x[i]=='1'):
+            out += exp
+        exp = exp * 2
+        i = i - 1
+    if(x[0]=='1'):
+        out -= 2**k
+    return out
+
+string_in = sys.argv[1]
+f = open(string_in, "r")
+points = f.read()
+f.close()
+
+points_list = points.split('\n')
+points_list.pop(-1)
+
+pt_real = []
+pt_imag = []
+
+for i in points_list:
+    pt_real.append(double(i[:17]))
+    pt_imag.append(double(i[18:]))
+
+string_out = string_in[0:-4] + "_double" + string_in[-4:]
+f = open(string_out, "w")
+i = 0
+while(i<len(pt_real)):
+    if(pt_imag[i]<0):
+        f.write(str(pt_real[i]) + str(pt_imag[i]) + "i" + '\n')
+    else:
+        f.write(str(pt_real[i]) + "+" + str(pt_imag[i]) + "i" + '\n')
+    i += 1
+f.close()
